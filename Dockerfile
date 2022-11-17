089626090db96203b13c61ef5173996ea49c93f2
zis9564 <zis9564@gmail.com>
2022-11-17 14:45:38 +0300

    implemented triggers for db

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:11-jdk
 VOLUME /tmp
 
-COPY api-gateway/build/libs/gp-gateway-0.1.0.jar app.jar
+COPY api-gateway/build/libs/api-gateway.jar app.jar
 
 ENTRYPOINT ["java","-jar","/app.jar",""]
--- src/main/java/com/zis/guavapay/ApiGatewayApplication.java
+++ src/main/java/com/zis/guavapay/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.zis.guavapay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
